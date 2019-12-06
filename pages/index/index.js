bf6301ec01affab0e69cdb10bb02798d245414e5
1463353697 <1463353697@qq.com>
2019-12-06 14:07:02 +0800

    some change

--- pages/index/index.js
+++ pages/index/index.js
@@ -7,7 +7,86 @@ Page({
     motto: 'Hello World',
     userInfo: {},
     hasUserInfo: false,
-    canIUse: wx.canIUse('button.open-type.getUserInfo')
+    canIUse: wx.canIUse('button.open-type.getUserInfo'),
+
+
+
+
+    //这是自己后来加的
+    focus: false,
+    inputValue: '',
+
+    // 这是轮播图片用的
+    imgUrls: [
+
+      {
+
+        link: '/pages/index/index',
+
+        url: '../../public/lunbo1.png'
+
+      }, {
+
+        link: '/pages/logs/logs',
+
+        url: '../../public/lunbo2.jpg'
+
+      }, {
+
+        link: '/pages/index/index',
+
+        url: '../../public/lunbo3.png'
+
+      }
+
+    ],
+
+    indicatorDots: true,  //小点
+
+    autoplay: true,  //是否自动轮播
+
+    interval: 3000,  //间隔时间
+
+    duration: 3000,  //滑动时间
+
+
+    // 这是导航菜单栏用的
+    menu:{ 
+      imgUrls:[
+        '../../public/fenlei1.png', 
+        '../../public/fenlei2.png', 
+        '../../public/fenlei3.png', 
+        '../../public/fenlei4.png', 
+        '../../public/fenlei5.png', 
+        '../../public/fenlei6.png', 
+        '../../public/fenlei7.png', 
+        '../../public/fenlei8.png',
+       
+      ], 
+      descs:[ 
+        '早餐', 
+        '面食', 
+        '烧烤', 
+        '甜品饮品', 
+        '套饭炒饭', 
+        '冒菜', 
+        '汉堡炸鸡', 
+        '保温柜' 
+       
+      ] 
+
+    },
+    recommendList:{
+      src:[
+        '../../public/recommend1.png',
+        '../../public/recommend2.png'
+
+      ],
+      name:[
+        "炸酱面",
+        "冒菜"
+      ]
+    }
   },
   //事件处理函数
   bindViewTap: function() {
@@ -49,6 +128,14 @@ Page({
     this.setData({
       userInfo: e.detail.userInfo,
       hasUserInfo: true
+    })
+  },
+
+
+  //这个地方的也是自己加的
+  bindButtonTap: function () {
+    this.setData({
+      focus: true
     })
   }
 })
